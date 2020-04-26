dc50c5469477c144add223fa89367d158c06b5e1
Niklas Schildhauer <niklasschildhauer@googlemail.com>
2020-04-26 21:49:14 +0200

    1 - init express server

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+
+const port = 8000;
+const app = express();
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
