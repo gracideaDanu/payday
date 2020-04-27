dda7632ffefa0dfa1943163ad9a4c11c1d811eca
Niklas Schildhauer <ns107@hdm-stuttgart.de>
2020-04-27 19:43:18 +0200

    8 - Add Font Baloo and fixed icon bug

--- frontend/payday/src/components/ui-elements/input/input.js
+++ frontend/payday/src/components/ui-elements/input/input.js
@@ -1,40 +1,48 @@
-import React from 'react';
-import PasswordIcon from '../../../assets/icons/icon_password.svg';
-import MailIcon from '../../../assets/icons/icon_mail.svg';
-import TextIcon from '../../../assets/icons/icon_name.svg';
+import React from "react";
+import PasswordIcon from "../../../assets/icons/icon_password.svg";
+import MailIcon from "../../../assets/icons/icon_mail.svg";
+import TextIcon from "../../../assets/icons/icon_name.svg";
 
-import './input.css'
+import "./input.css";
 
 const input = (props) => {
-    // var cssClasses = [props.classes];
+  // var cssClasses = [props.classes];
 
-    // if (props.invalid && props.shouldValidate && props.touched) {
-    //     cssClasses.push("invalid");
-    // } else {
-    //     cssClasses = [props.classes]
-    // }
-    var icon = null;
-    switch (props.elementType) {
-        case 'password': icon = PasswordIcon;
-        case 'email': icon = MailIcon;
-        case 'text': icon = TextIcon;
-        default: icon = TextIcon;
-    }
+  // if (props.invalid && props.shouldValidate && props.touched) {
+  //     cssClasses.push("invalid");
+  // } else {
+  //     cssClasses = [props.classes]
+  // }
+  var icon = null;
+  switch (props.elementConfig.type) {
+    case "password":
+      icon = PasswordIcon;
+      break;
+    case "email":
+      icon = MailIcon;
+      break;
+    case "text":
+      icon = TextIcon;
+      break;
+    default:
+      icon = TextIcon;
+  }
 
-    return (
-        <div className="ui-input-container">
-            <div className="ui-input-img-wrapper">
-                <img src={icon} />
-                <input
-                    className="ui-input"
-                    {...props.elementConfig}
-                    value={props.value}
-                    // placeholder={props.title}
-                    onChange={props.changed} />
-            </div>
-            <div class="ui-input-line"></div>
-        </div>
-    )
-}
+  return (
+    <div className="ui-input-container">
+      <div className="ui-input-img-wrapper">
+        <img src={icon} />
+        <input
+          className="ui-input"
+          {...props.elementConfig}
+          value={props.value}
+          // placeholder={props.title}
+          onChange={props.changed}
+        />
+      </div>
+      <div class="ui-input-line"></div>
+    </div>
+  );
+};
 
 export default input;
--- frontend/payday/src/container/authentication/auth.js
+++ frontend/payday/src/container/authentication/auth.js
@@ -1,242 +1,234 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Button from '../../components/ui-elements/buttons/button';
-import Input from '../../components/ui-elements/input/input';
-import './auth.css'
-import Logo from '../../assets/icons/logo.svg';
+import Button from "../../components/ui-elements/buttons/button";
+import Input from "../../components/ui-elements/input/input";
+import "./auth.css";
+import Logo from "../../assets/icons/logo.svg";
 
 const signInState = {
-    controls: {
-        email: {
-            elementType: 'input',
-            elementConfig: {
-                type: 'email',
-                placeholder: 'E-Mail Adresse'
-            },
-            icon: '../../../assets/icons/icon_mail.svg',
-            value: '',
-            validation: {
-                required: true,
-                isEmaiil: true
-            },
-            valid: false,
-            touched: false
-        },
-        password: {
-            elementType: 'input',
-            elementConfig: {
-                type: 'password',
-                placeholder: 'Passwort eingeben'
-            },
-            icon: '../../../assets/icons/icon_mail.svg',
-            value: '',
-            validation: {
-                required: true,
-                minLength: 6
-            },
-            valid: false,
-            touched: false
-        }
-
+  controls: {
+    email: {
+      elementType: "input",
+      elementConfig: {
+        type: "email",
+        placeholder: "E-Mail Adresse",
+      },
+      value: "",
+      validation: {
+        required: true,
+        isEmaiil: true,
+      },
+      valid: false,
+      touched: false,
     },
-    isSignUp: false
+    password: {
+      elementType: "input",
+      elementConfig: {
+        type: "password",
+        placeholder: "Passwort eingeben",
+      },
+      value: "",
+      validation: {
+        required: true,
+        minLength: 6,
+      },
+      valid: false,
+      touched: false,
+    },
+  },
+  isSignUp: false,
 };
 
 const signUpState = {
-    controls: {
-        username: {
-            elementType: 'username',
-            elementConfig: {
-                type: 'text',
-                placeholder: 'Username eingeben'
-            },
-            icon: '../../../assets/icons/icon_mail.svg',
-            value: '',
-            validation: {
-                required: true,
-                minLength: 6
-            },
-            valid: false,
-            touched: false
-        },
-        forename: {
-            elementType: 'name',
-            elementConfig: {
-                type: 'text',
-                placeholder: 'Vorname eingeben'
-            },
-            icon: '../../../assets/icons/icon_mail.svg',
-            value: '',
-            validation: {
-                required: true,
-                minLength: 2
-            },
-            valid: false,
-            touched: false
-        },
-        surname: {
-            elementType: 'surname',
-            elementConfig: {
-                type: 'text',
-                placeholder: 'Nachname eingeben'
-            },
-            icon: '../../../assets/icons/icon_mail.svg',
-            value: '',
-            validation: {
-                required: true,
-                minLength: 2
-            },
-            valid: false,
-            touched: false
-        },
-        email: {
-            elementType: 'input',
-            elementConfig: {
-                type: 'email',
-                placeholder: 'E-Mail Adresse'
-            },
-            icon: '../../../assets/icons/icon_mail.svg',
-            value: '',
-            validation: {
-                required: true,
-                isEmaiil: true
-            },
-            valid: false,
-            touched: false
-        },
-        password: {
-            elementType: 'input',
-            elementConfig: {
-                type: 'password',
-                placeholder: 'Passwort eingeben'
-            },
-            icon: '../../../assets/icons/icon_mail.svg',
-            value: '',
-            validation: {
-                required: true,
-                minLength: 6
-            },
-            valid: false,
-            touched: false
-        },
-
-
+  controls: {
+    username: {
+      elementType: "username",
+      elementConfig: {
+        type: "text",
+        placeholder: "Username eingeben",
+      },
+      value: "",
+      validation: {
+        required: true,
+        minLength: 6,
+      },
+      valid: false,
+      touched: false,
     },
-    isSignUp: true
+    forename: {
+      elementType: "name",
+      elementConfig: {
+        type: "text",
+        placeholder: "Vorname eingeben",
+      },
+      value: "",
+      validation: {
+        required: true,
+        minLength: 2,
+      },
+      valid: false,
+      touched: false,
+    },
+    surname: {
+      elementType: "surname",
+      elementConfig: {
+        type: "text",
+        placeholder: "Nachname eingeben",
+      },
+      value: "",
+      validation: {
+        required: true,
+        minLength: 2,
+      },
+      valid: false,
+      touched: false,
+    },
+    email: {
+      elementType: "input",
+      elementConfig: {
+        type: "email",
+        placeholder: "E-Mail Adresse",
+      },
+      value: "",
+      validation: {
+        required: true,
+        isEmaiil: true,
+      },
+      valid: false,
+      touched: false,
+    },
+    password: {
+      elementType: "input",
+      elementConfig: {
+        type: "password",
+        placeholder: "Passwort eingeben",
+      },
+      value: "",
+      validation: {
+        required: true,
+        minLength: 6,
+      },
+      valid: false,
+      touched: false,
+    },
+  },
+  isSignUp: true,
 };
 
-
 class Auth extends Component {
+  state = signInState;
 
-    state = signInState
+  switchAuthModeHandler = () => {
+    if (this.state.isSignUp) {
+      this.setState(signInState);
+    } else {
+      this.setState(signUpState);
+    }
+  };
 
-    switchAuthModeHandler = () => {
-        if (this.state.isSignUp) {
-            this.setState(signInState);
-        } else {
-            this.setState(signUpState);
-        }
-
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (!rules) {
+      return true;
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        if (!rules) {
-            return true;
-        }
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        return isValid;
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
     }
 
-    inputChangedHandler = (event, controlName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [controlName]: {
-                ...this.state.controls[controlName],
-                value: event.target.value,
-                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
-                touched: true
-            }
-        };
-        this.setState({ controls: updatedControls });
-        console.log(this.state)
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
     }
 
-    submitHandler = (event) => {
-        // event.preventDefault();
-        // if (this.state.isSignUp) {
-        //     const signUpData = {
-        //         username: this.state.controls.username.value,
-        //         name: this.state.controls.forename.value,
-        //         surname: this.state.controls.surname.value,
-        //         email: this.state.controls.email.value,
-        //         password: this.state.controls.password.value
-        //     }
-        //     this.props.onAuth(true, signUpData);
-        // } else {
-        //     const signInData = {
-        //         email: this.state.controls.email.value,
-        //         password: this.state.controls.password.value
-        //     }
-        //     this.props.onAuth(false, signInData);
-        // }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
     }
 
-    render() {
-        const formElementsArray = [];
-        for (let key in this.state.controls) {
-            formElementsArray.push({
-                id: key,
-                config: this.state.controls[key]
-            })
-        }
-
-        const form = formElementsArray.map(formElement => (
-            <Input
-                key={formElement.id}
-                elementType={formElement.config.elementType}
-                elementConfig={formElement.config.elementConfig}
-                value={formElement.config.value}
-                invalid={!formElement.config.valid}
-                shouldValidate={formElement.config.validation}
-                touched={formElement.config.touched}
-                changed={(event) => this.inputChangedHandler(event, formElement.id)} />
-        ))
-        return (
-            <div className="auth-container">
-                <img src={Logo} className="auth-container-logo"></img>
-                <form>
-                    {form}
-                </form>
-                <div className="auth-button-container">
-                    <Button clicked={this.submitHandler} btnStyle="blue">Submit </Button>
-                    <Button clicked={this.switchAuthModeHandler} btnStyle="mint">Switch to {this.state.isSignUp ? 'Sign in' : 'Sign up'} </Button></div>
-            </div>
-        )
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
     }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true,
+      },
+    };
+    this.setState({ controls: updatedControls });
+    console.log(this.state);
+  };
+
+  submitHandler = (event) => {
+    // event.preventDefault();
+    // if (this.state.isSignUp) {
+    //     const signUpData = {
+    //         username: this.state.controls.username.value,
+    //         name: this.state.controls.forename.value,
+    //         surname: this.state.controls.surname.value,
+    //         email: this.state.controls.email.value,
+    //         password: this.state.controls.password.value
+    //     }
+    //     this.props.onAuth(true, signUpData);
+    // } else {
+    //     const signInData = {
+    //         email: this.state.controls.email.value,
+    //         password: this.state.controls.password.value
+    //     }
+    //     this.props.onAuth(false, signInData);
+    // }
+  };
+
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key],
+      });
+    }
+
+    const form = formElementsArray.map((formElement) => (
+      <Input
+        key={formElement.id}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={(event) => this.inputChangedHandler(event, formElement.id)}
+      />
+    ));
+    return (
+      <div className="auth-container">
+        <img src={Logo} className="auth-container-logo"></img>
+        <form>{form}</form>
+        <div className="auth-button-container">
+          <Button clicked={this.submitHandler} btnStyle="blue">
+            Submit{" "}
+          </Button>
+          <Button clicked={this.switchAuthModeHandler} btnStyle="mint">
+            Switch to {this.state.isSignUp ? "Sign in" : "Sign up"}{" "}
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Auth;
-
