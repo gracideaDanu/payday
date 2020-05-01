740f14f19204a05dae35a9ae2643803d9a770c54
Niklas Schildhauer <ns107@hdm-stuttgart.de>
2020-05-01 10:48:29 +0200

    4 - create table script for postrgessql table

--- backend/data/init-db.sql
+++ backend/data/init-db.sql
@@ -1,17 +1,6 @@
-CREATE TABLE test (
-    id integer NOT NULL,
-    conf_id integer NOT NULL,
-    option_key character varying NOT NULL,
-    ausstatt_kz character varying NOT NULL,
-    ausstatt_alt character varying NOT NULL,
-    option_category_id character varying NOT NULL,
-    option_type_id character varying 
-);
-
-
 CREATE TABLE public."User"
 (
-    "ID" SERIAL UNIQUE,
+    "Id" SERIAL UNIQUE,
     "Username" VARCHAR NOT NULL,
     "FirstName" VARCHAR NOT NULL,
     "LastName" VARCHAR NOT NULL,
@@ -19,5 +8,68 @@ CREATE TABLE public."User"
     "Password" VARCHAR NOT NULL,
     "Image" VARCHAR,
     "CreatedAt" timestamp NOT NULL DEFAULT NOW(),
-    CONSTRAINT "User_pkey" PRIMARY KEY ("ID")
+    CONSTRAINT "PK_User" PRIMARY KEY ("Id")
 );
+
+
+CREATE TABLE public."Expense"
+(
+    "Id" SERIAL UNIQUE,
+    "Title" VARCHAR NOT NULL,
+    "Costs" VARCHAR NOT NULL,
+    "Category" VARCHAR NOT NULL,
+    "Group" VARCHAR NOT NULL,
+    "Owner" VARCHAR,
+    "CreatedAt" timestamp NOT NULL DEFAULT NOW(),
+    CONSTRAINT "PK_Expense" PRIMARY KEY ("Id")
+);
+
+CREATE TABLE public."ExpenseParticipants"
+(
+    "Id" SERIAL UNIQUE,
+    "UserId" INTEGER NOT NULL,
+    "ExpenseId" INTEGER NOT NULL,
+    CONSTRAINT "PK_ExpenseParticipants" PRIMARY KEY ("Id")
+);
+
+
+CREATE TABLE public."Group"
+(
+    "Id" SERIAL UNIQUE,
+    "Name" VARCHAR NOT NULL,
+    "CreatedAt" timestamp NOT NULL DEFAULT NOW(),
+    CONSTRAINT "PK_Group" PRIMARY KEY ("Id")
+);
+
+CREATE TABLE public."GroupUsers"
+(
+    "Id" SERIAL UNIQUE,
+    "UserId" INTEGER NOT NULL,
+    "GroupId" INTEGER NOT NULL,
+    CONSTRAINT "PK_GroupUsers" PRIMARY KEY ("Id")
+);
+
+
+CREATE TABLE public."Category"
+(
+    "Id" SERIAL UNIQUE,
+    "Name" VARCHAR NOT NULL,
+    "Image" VARCHAR NOT NULL,
+    "CreatedAt" timestamp NOT NULL DEFAULT NOW(),
+    CONSTRAINT "OK_Category" PRIMARY KEY ("Id")
+);
+
+
+ALTER TABLE  public."GroupUsers"
+    ADD CONSTRAINT "FK_GroupUsers_User" FOREIGN KEY ("UserId") REFERENCES public."User"("Id");
+
+ALTER TABLE  public."GroupUsers"
+    ADD CONSTRAINT "FK_GroupUsers_Group" FOREIGN KEY ("GroupId") REFERENCES public."Group"("Id");
+
+
+ALTER TABLE  public."ExpenseParticipants"
+    ADD CONSTRAINT "FK_ExpenseParticipants_User" FOREIGN KEY ("UserId") REFERENCES public."User"("Id");
+
+ALTER TABLE  public."ExpenseParticipants"
+    ADD CONSTRAINT "FK_ExpenseParticipants_Expense" FOREIGN KEY ("ExpenseId") REFERENCES public."Expense"("Id");
+
