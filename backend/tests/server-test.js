5dfc08cd70eacc524016a9c360a73f38c212ebf2
Niklas Schildhauer <ns107@hdm-stuttgart.de>
2020-05-10 11:18:08 +0200

    23 - api test cases implemented

--- backend/tests/server-test.js
+++ backend/tests/server-test.js
@@ -17,8 +17,7 @@ chai.use(chaiHttp);
 
 before(async () => {
   // var queryUser = initScript;
-  var queryUser = `DELETE FROM public."User"; DELETE FROM public."Category";`;
-  console.log(queryUser);
+  var queryUser = `DELETE FROM public."User"; DELETE FROM public."Category"; DELETE FROM public."ExpenseParticipants"; DELETE FROM public."GroupUsers"; DELETE FROM public."Expense"; DELETE FROM public."Group"; DELETE FROM public."User";`;
   try {
     await database.query(queryUser);
   } catch (e) {
@@ -40,6 +39,8 @@ describe("Test", () => {
     });
   });
 });
+
+var sessionToken = null;
 
 describe("Authentication test", () => {
   describe("Post signup", () => {
@@ -147,51 +148,336 @@ describe("Authentication test", () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.have.property("token");
+          sessionToken = res.body.token;
           done();
         });
     });
   });
 });
 
-// describe("API test", () => {
-//   describe("Post signup", () => {
-//     it("should signup user and return token", (done) => {
-//       chai
-//         .request(server)
-//         .post("/signup/")
-//         .set("content-type", "application/json")
-//         .send({
-//           username: "niklas",
-//           name: "Niklas",
-//           surname: "Schildhauer",
-//           email: "niklas@icloud.com",
-//           password: "Password",
-//         })
-//         .end((err, res) => {
-//           res.should.have.status(200);
-//           res.body.should.have.property("token");
-//           done();
-//         });
-//     });
-//   });
-//   describe("Post login", () => {
-//     it("should login user and return token", (done) => {
-//       chai
-//         .request(server)
-//         .post("/login/")
-//         .set("content-type", "application/json")
-//         .send({
-//           email: "niklas@icloud.com",
-//           password: "Password",
-//         })
-//         .end((err, res) => {
-//           res.should.have.status(200);
-//           res.body.should.have.property("token");
-//           done();
-//         });
-//     });
-//   });
-// });
+describe("API test", () => {
+  var users = null;
+  describe("GET users", () => {
+    it("should return all users without his user (array with two users)", (done) => {
+      chai
+        .request(server)
+        .get("/api/users")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property("users");
+          res.body.users.should.have.length(2);
+          users = res.body.users;
+          done();
+        });
+    });
+  });
+
+  describe("Post category", () => {
+    it("should create a new category and return Status code 201", (done) => {
+      chai
+        .request(server)
+        .post("/api/category")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          name: "Kegeln",
+          image: "🎳",
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it("should create a new category and return Status code 201", (done) => {
+      chai
+        .request(server)
+        .post("/api/category")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          name: "Essen",
+          image: "🍔",
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it("shouldn't create a category and return status code 400", (done) => {
+      chai
+        .request(server)
+        .post("/api/category")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          name: "Trinken",
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+  });
+
+  var categories = null;
+  describe("GET categories", () => {
+    it("should return all categories", (done) => {
+      chai
+        .request(server)
+        .get("/api/categories")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property("categories");
+          res.body.groups.should.have.length(2);
+          categories = res.body.categories;
+          done();
+        });
+    });
+  });
+
+  describe("Post Group", () => {
+    it("should create a new group with niklas, pascsal and max and return Status code 201", (done) => {
+      chai
+        .request(server)
+        .post("/api/group")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          name: "Kegelgruppe",
+          participants: [users[0].Id, users[1].Id],
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it("should create a new group with niklas and pascsal and return Status code 201", (done) => {
+      chai
+        .request(server)
+        .post("/api/group")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          name: "Trinkgruppe",
+          participants: [users[0].Id],
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it("shouldn't create a group and return status code 400", (done) => {
+      chai
+        .request(server)
+        .post("/api/group")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          name: "Trinkgruppe2",
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+  });
+
+  var group = null;
+  describe("GET groups", () => {
+    it("should return all groups from the logged in user", (done) => {
+      chai
+        .request(server)
+        .get("/api/groups")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property("groups");
+          res.body.groups.should.have.length(2);
+          group = res.body.group[0];
+          done();
+        });
+    });
+  });
+
+  describe("Post expense", () => {
+    it("should post a expense to certain group", (done) => {
+      chai
+        .request(server)
+        .post("/api/expense")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          title: "Getränkekauf",
+          costs: "12,99",
+          category: categories[0].Id,
+          group: group.Id,
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it("should post a expense to certain group", (done) => {
+      chai
+        .request(server)
+        .post("/api/expense")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          title: "Essen",
+          costs: "20,99",
+          category: categories[1].Id,
+          group: group.Id,
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it("shouldn't post a expense to certain group", (done) => {
+      chai
+        .request(server)
+        .post("/api/expense")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          name: "Essen",
+          costs: "20,99",
+          category: categories[1].Id,
+          group: group.Id,
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+    it("shouldn't post a expense to certain group", (done) => {
+      chai
+        .request(server)
+        .post("/api/expense")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          title: "Essen",
+          category: categories[1].Id,
+          group: group.Id,
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+    it("shouldn't post a expense to certain group", (done) => {
+      chai
+        .request(server)
+        .post("/api/expense")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          title: "Essen",
+          costs: "12,99",
+          group: group.Id,
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+    it("shouldn't post a expense to certain group", (done) => {
+      chai
+        .request(server)
+        .post("/api/expense")
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          title: "Essen",
+          costs: "12,99",
+          category: categories[1].Id,
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+  });
+
+  var expenses = null;
+  describe("GET expenses", () => {
+    it("should return all expenses from certain group", (done) => {
+      chai
+        .request(server)
+        .get("/api/expenses/" + group.Id)
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property("expenses");
+          res.body.groups.should.have.length(2);
+          expenses = res.body.expenses[0];
+          done();
+        });
+    });
+  });
+
+  describe("PUT expense", () => {
+    it("should replace a expense", (done) => {
+      chai
+        .request(server)
+        .put("/api/expense/" + expenses[0].Id)
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send({
+          title: "Essen",
+          costs: "18,99",
+          category: categories[1].Id,
+          group: group.Id,
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe("DELETE expense", () => {
+    it("should delete a certain expense", (done) => {
+      chai
+        .request(server)
+        .delete("/api/expense/" + expenses[0].Id)
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe("DELETE group", () => {
+    it("should delete a certain group", (done) => {
+      chai
+        .request(server)
+        .delete("/api/expense/" + group.Id)
+        .set("content-type", "application/json")
+        .set("authentication", "Bearer " + sessionToken)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+});
 
 // describe('useless api endpoint', function () {
 //   var token;
